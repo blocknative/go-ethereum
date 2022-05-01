dfd2a2e00c583baba72922f6926bacfc84ed362d
Dmarz <danmarzec@icloud.com>
2022-04-30 20:38:56 -0400

    test more tx uses

--- eth/filters/peers_api.go
+++ eth/filters/peers_api.go
@@ -303,11 +303,12 @@ func (api *PublicFilterAPI) NewPendingTransactionsWithPeers(ctx context.Context)
 			case hashes := <-txHashes:
 				for _, h := range hashes {
 					log.Info("(Current Source) NewPendingTransactionsWithPeers", "hash", h)
-					fullTx := api.backend.GetPoolTransaction(h)
-					log.Info("(Current Source) NewPendingTransactionsWithPeers", "fullTx", fullTx)
+
 					peerid, _ := txPeerMap.Get(h)
 					p2pts, _ := tsMap.Get(h)
 					peer, _ := peerIDMap.Load(peerid)
+					fullTxWithPeer := withPeer{Value: newRPCPendingTransaction(api.backend.GetPoolTransaction(h)), Peer: peer, Time: time.Now().UnixNano(), P2PTime: p2pts}
+					log.Info("(Current Source) NewPendingTransactionsWithPeers", "fullTxWithPeer", fullTxWithPeer)
 					notifier.Notify(rpcSub.ID, withPeer{Value: newRPCPendingTransaction(api.backend.GetPoolTransaction(h)), Peer: peer, Time: time.Now().UnixNano(), P2PTime: p2pts})
 				}
 			case <-rpcSub.Err():
