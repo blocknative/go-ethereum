32e76b8a9cf2b56116d8e64af8dea4a60e5eb01c
Alexander Campbell <alexander.wesley.campbell@gmail.com>
2022-06-17 17:56:04 +1000

    Stop hex spam

--- eth/tracers/native/goCallTracer.go
+++ eth/tracers/native/goCallTracer.go
@@ -76,7 +76,7 @@ func (tracer *CallTracer) GetResult() (json.RawMessage, error) {
 	if err != nil {
 		return nil, err
 	}
-	fmt.Println("DEBUG | tracer.callStack[0]: ", res)
+	fmt.Println("DEBUG | tracer.callStack[0]: ", json.RawMessage(res))
 	// fmt.Println("DEBUG | tracer.callStack[1]: ", res1)
 	return json.RawMessage(res), tracer.reason
 	// return tracer.callStack[0], nil // TODO ALEX: this was the old return type, cannot do in new structure
