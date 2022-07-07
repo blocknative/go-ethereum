e9fd9892cc28be7b8d20c671caa8b096a342ace9
Alexander Campbell <alexander.wesley.campbell@gmail.com>
2022-07-07 22:15:59 +1000

    Remove comments, add debugs for revert reasons

--- eth/tracers/native/txnOpCodeTracer.go
+++ eth/tracers/native/txnOpCodeTracer.go
@@ -8,6 +8,7 @@ import (
 	"sync/atomic"
 	"time"
 
+	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/vm"
 	"github.com/ethereum/go-ethereum/eth/tracers"
@@ -35,20 +36,20 @@ type callFrameBN struct {
 	gasCost uint64
 	Time    string `json:"time,omitempty"`
 
-	// TODO ALEX: Investigate precompiles usage, could reduce latency as they are "those are just fancy opcodes" - 4byte.go
+	// TODO: Investigate precompiles usage, could reduce latency as they are "those are just fancy opcodes" - 4byte.go
 	// activePrecompiles []common.Address // Updated on CaptureStart based on given rules
 
 }
 
 // txnOpCodeTracer is a go implementation of the Tracer interface which
-// only returns a smilled trace of a transaction consisting of transaction
+// only returns a restricted trace of a transaction consisting of transaction
 // op codes and relevant gas data.
 // This is intended for Blocknative usage.
 type txnOpCodeTracer struct {
-	env       *vm.EVM       // TODO ALEX: is this used when going step by step?
-	callStack []callFrameBN // TODO ALEX: this is being imported from call.go, which should be fine!
+	env       *vm.EVM       // EVM context for execution of transaction to occur within
+	callStack []callFrameBN // Data structure for op codes making up our trace
 	interrupt uint32        // Atomic flag to signal execution interruption
-	reason    error         // Textual reason for the interruption
+	reason    error         // Textual reason for the interruption (not always specific for us)
 }
 
 // newtxnOpCodeTracer returns a new txnOpCodeTracer tracer.
@@ -60,8 +61,6 @@ func newtxnOpCodeTracer(ctx *tracers.Context) tracers.Tracer {
 
 // GetResult returns an empty json object.
 func (t *txnOpCodeTracer) GetResult() (json.RawMessage, error) {
-	// TODO ALEX: Ensure this result is correct, place a bunch of fmt.Println("DEBUG | ")
-	fmt.Println("DEBUG | my tracer callstack len: ", len(t.callStack))
 	if len(t.callStack) != 1 {
 		return nil, errors.New("incorrect number of top-level calls")
 	}
@@ -85,38 +84,40 @@ func (t *txnOpCodeTracer) CaptureStart(env *vm.EVM, from common.Address, to comm
 		Value: bigToHex(value),
 	}
 	if create {
-		// fmt.Println("DEBUG | found CREATE on CaptureStart")
+		// TODO: Here we can note creation of contracts for potential future tracing
 		t.callStack[0].Type = "CREATE"
 	}
-
-	// TODO ALEX: Look at `Compute intrinsic gas` line in Js tracer, understand how gas changed and if current tracing deals with it!
 }
 
 // CaptureEnd is called after the call finishes to finalize the tracing.
 func (t *txnOpCodeTracer) CaptureEnd(output []byte, gasUsed uint64, time time.Duration, err error) {
 	// Collect final gasUsed
-	// TODO ALEX: Add metrics to bn-server for all tracers behind config flag for datat team to analyze this!
 	t.callStack[0].GasUsed = uintToHex(gasUsed)
 
 	// Add total time duration for this trace request
 	// TODO ALEX: Double check this time here!
+	fmt.Println("DEBUG | time being saved on captureEnd: ", fmt.Sprintf("%v", time))
 	t.callStack[0].Time = fmt.Sprintf("%v", time)
 
 	// This is the final output of a call
-	// TODO QUESTION: Is this the outmost layer call final, or is this needed for nested calls?
-	// TODO ENHANCE: This isn't required for Blocknative purposes, could slim this down by cutting
 	if err != nil {
 		t.callStack[0].Error = err.Error()
 		if err.Error() == "execution reverted" && len(output) > 0 {
 			t.callStack[0].Output = bytesToHex(output)
+			// TODO ALEX: test this revert reason here
+			revertReason, _ := abi.UnpackRevert(output)
+
+			fmt.Println("DEBUG | output converted with UnpackRevert: ", fmt.Sprintf("%v", revertReason))
+
 		}
 	} else {
+		// TODO: This output is for the originally called contract, we can use the ABI to decode this for useful information
+		// ie: there are error types in ABIs since 0.8.4 which will turn up here. (I think)
 		t.callStack[0].Output = bytesToHex(output)
 	}
 }
 
 // CaptureState implements the EVMLogger interface to trace a single step of VM execution.
-// TODO: NOT IMPLEMENTED
 func (t *txnOpCodeTracer) CaptureState(pc uint64, op vm.OpCode, gas, cost uint64, scope *vm.ScopeContext, rData []byte, depth int, err error) {
 	// TODO ALEX: this is where we log specific op codes!
 
@@ -129,25 +130,21 @@ func (t *txnOpCodeTracer) CaptureState(pc uint64, op vm.OpCode, gas, cost uint64
 		}
 	}()
 
-	// TODO ALEX: Here I must do a switch statement on each op code we are interested in
-	// Op codes we like at BN, on different lines indicating different handling (via AusIV tracer)
+	// TODO Here we can check for specific op codes that may interest us
+	// Op codes we like at BN
 	// CREATE, CREATE2
 	// SELFDESTRUCT
-	// CALL, CALLCODE, DELEGATECALL, STATICCALL
+	// CALL, CALLCODE, DELEGATECALL, STATICCALL (picked up by CaptureEnter)
 	// REVERT
-
-	// TODO ALEX: look up other op codes we may want to use, there might be some in the CaptureState for prestate.go?
-
-	// TODO ALEX: Maybe this CaptureState doesn't need to check against call, since CaptureEnter seems to do this? Or is that only on the first call in the stack? hmmm
 }
 
 // CaptureFault implements the EVMLogger interface to trace an execution fault.
-// TODO: NOT IMPLEMENTED
 func (t *txnOpCodeTracer) CaptureFault(pc uint64, op vm.OpCode, gas, cost uint64, _ *vm.ScopeContext, depth int, err error) {
+	// TODO: This should now add faults to the return value for bn-server logging
+	fmt.Printf("DEBUG | error from CaptureFault: op=%v, err=%v", op, err)
 }
 
 // CaptureEnter is called when EVM enters a new scope (via call, create or selfdestruct).
-// TODO: NOT IMPLEMENTED
 func (t *txnOpCodeTracer) CaptureEnter(typ vm.OpCode, from common.Address, to common.Address, input []byte, gas uint64, value *big.Int) {
 	// Skip if tracing was interrupted
 	if atomic.LoadUint32(&t.interrupt) > 0 {
@@ -155,8 +152,7 @@ func (t *txnOpCodeTracer) CaptureEnter(typ vm.OpCode, from common.Address, to co
 		return
 	}
 
-	// TODO ALEX: Ensure this is not doubling up on information from a possible other capture function
-	// Where is this exactly being called?
+	// Apart from the starting call detected by CaptureStart, here we track every new transaction opcode
 	call := callFrameBN{
 		Type:  typ.String(),
 		From:  addrToHex(from),
@@ -170,12 +166,8 @@ func (t *txnOpCodeTracer) CaptureEnter(typ vm.OpCode, from common.Address, to co
 
 // CaptureExit is called when EVM exits a scope, even if the scope didn't
 // execute any code.
-// TODO: NOT IMPLEMENTED
 func (t *txnOpCodeTracer) CaptureExit(output []byte, gasUsed uint64, err error) {
-	// TODO ALEX: Here in call.go, we pop the call from the stack
-	// Does this delete a history of calls? That's what we want though?
 
-	// TODO ALEX: double check all the logic here!!! None has been proof read!
 	size := len(t.callStack)
 	if size <= 1 {
 		return
@@ -197,13 +189,9 @@ func (t *txnOpCodeTracer) CaptureExit(output []byte, gasUsed uint64, err error) 
 	t.callStack[size-1].Calls = append(t.callStack[size-1].Calls, call)
 }
 
-// TODO: NOT IMPLEMENTED
 func (*txnOpCodeTracer) CaptureTxStart(gasLimit uint64) {
-	// TODO ALEX: Does the gasLimit need to be attached here, I think it is already there?
 }
 
-// TODO: NOT IMPLEMENTED
-// TODO ALEX: Might not need this, check where in geth we may ever need to call this!
 func (*txnOpCodeTracer) CaptureTxEnd(restGas uint64) {}
 
 // Stop terminates execution of the tracer at the first opportune moment.
