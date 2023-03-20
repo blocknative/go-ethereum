1c79ea53a112f1b5e15c3cf9bb7558e39efcf8b7
Alexander Campbell <alexander.wesley.campbell@gmail.com>
2023-03-21 00:26:08 +1100

    First commit, set up tracer for optional value / storage tracking, with no decoding yet

--- eth/tracers/blocknative/blocknative.go
+++ eth/tracers/blocknative/blocknative.go
@@ -2,7 +2,9 @@ package blocknative
 
 import (
 	"encoding/json"
+	"math/big"
 	"time"
+
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/vm"
 )
@@ -22,16 +24,18 @@ type Tracer interface {
 
 // TracerOpts configure the tracer to save or ignore various aspects of a transaction execution.
 type TracerOpts struct {
-	Logs bool `json:"logs"`
+	Logs          bool `json:"logs"`
+	NetBalChanges bool `json:"diffMode"`
 }
 
 // Trace contains all the accumulated details of a transaction execution.
 type Trace struct {
 	CallFrame
-	BlockContext BlockContext `json:"blockContext"`
-	Logs         []CallLog    `json:"logs,omitempty"`
-	Time         string       `json:"time,omitempty"`
-	startTime    time.Time
+	BlockContext  BlockContext  `json:"blockContext"`
+	Logs          []CallLog     `json:"logs,omitempty"`
+	Time          string        `json:"time,omitempty"`
+	NetBalChanges NetBalChanges `json:"netBalChanges,omitempty"`
+	startTime     time.Time
 }
 
 // BlockContext contains information about the block we simulate transactions in.
@@ -69,4 +73,20 @@ type CallLog struct {
 
 	// Topics is a slice of up to 4 32byte words provided with the log.
 	Topics []common.Hash `json:"topics"`
+}
+
+// NetBalChanges represents the difference of value (ETH, erc20, erc721) after the transaction for all addresses
+type NetBalChanges struct {
+	Pre  state `json:"pre"`
+	Post state `json:"post"`
+	// Todo alex: if we want to track contract creations / deleteions we must add the idea here!
+}
+
+type state = map[common.Address]*account
+
+// Todo alex: This account structure will need some additional post processing mapping for token names and decoding of values
+type account struct {
+	Balance *big.Int                    `json:"balance,omitempty"`
+	Code    []byte                      `json:"code,omitempty"`
+	Storage map[common.Hash]common.Hash `json:"storage,omitempty"`
 }
