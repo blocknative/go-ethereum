919df8c1a422aee38826aaf1bad8df6b30cbdff2
Tyler Smith <mail@tcry.pt>
2023-06-08 05:36:27 -0400

    tweak: watch coinbase wallet proxy.

--- finch/constants.go
+++ finch/constants.go
@@ -27,10 +27,13 @@ const (
 	UniswapUniversalRouter = "0xEf1c6E67703c7BD7107eed8303Fbe6EC2554BF6B"
 	UniswapAutoRouter      = "0xe592427a0aece92de3edee1f18e0157c05861564"
 	UniswapV2Router        = "0x7a250d5630b4cf539739df2c5dacb4c659f2488d"
+
+	CoinbaseWalletProxy = "0xe66B31678d6C16E9ebf358268a790B763C133750"
 )
 
 var AMMRouters = []string{
 	UniswapUniversalRouter,
 	UniswapAutoRouter,
 	UniswapV2Router,
+	CoinbaseWalletProxy,
 }
