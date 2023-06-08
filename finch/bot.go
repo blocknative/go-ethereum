afdafe2c78b796d910e07c7f31bc10d3873b2eac
Tyler Smith <mail@tcry.pt>
2023-06-08 05:23:41 -0400

    tweak: only log receiving trade txs in debug.

--- finch/bot.go
+++ finch/bot.go
@@ -185,7 +185,7 @@ func (b *Bot) handleIncomingBlock(event core.ChainEvent) {
 func (b *Bot) handleIncomingTradeTx(tx *types.Transaction) error {
 	receivedTradeTxCounter.Inc()
 
-	log.Info("finch: received trade tx", "tx", tx.Hash().String())
+	log.Debug("finch: received trade tx", "tx", tx.Hash().String())
 
 	// Execute this transaction and get the receipt.
 	var (
