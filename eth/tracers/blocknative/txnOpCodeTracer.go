ea6f62e1f9d3aa0434744c7fe4ab10424d0d2081
Tyler Smith <mail@tcry.pt>
2023-06-20 03:24:36 -0400

    feat: Re-add lost tracer changes.

--- eth/tracers/blocknative/txnOpCodeTracer.go
+++ eth/tracers/blocknative/txnOpCodeTracer.go
@@ -97,18 +97,22 @@ func (t *txnOpCodeTracer) CaptureStart(env *vm.EVM, from common.Address, to comm
 		// TODO: Here we can note creation of contracts for potential future tracing
 		t.callStack[0].Type = "CREATE"
 	}
+
+	// Start timer
+	t.trace.startTime = time.Now()
 }
 
 // CaptureEnd is called after the call finishes to finalize the tracing.
 func (t *txnOpCodeTracer) CaptureEnd(output []byte, gasUsed uint64, err error) {
+	elapsedTime := time.Now().Sub(t.trace.startTime)
+
 	// Collect final gasUsed
 	t.callStack[0].GasUsed = uintToHex(gasUsed)
 
 	// Add total time duration for this trace request
 	// todo alex: need to find a better place to get time from the evm execution
 	// we can use t.trace.BlockContext.Time and current time to calculate this here!
-	now := time.Now()
-	t.trace.Time = fmt.Sprintf("%v", now.Sub(t.startTime).Seconds())
+	t.trace.Time = fmt.Sprintf("%v", elapsedTime)
 
 	// If the user wants the logs, grab them from the state
 	if t.opts.Logs {
--- eth/tracers/internal/tracetest/txnOpCodeTracer_test.go
+++ eth/tracers/internal/tracetest/txnOpCodeTracer_test.go
@@ -90,7 +90,7 @@ func testTxnOpCodeTracer(tracerName string, dirPath string, t *testing.T) {
 				t.Fatalf("failed to create call tracer: %v", err)
 			}
 			evm := vm.NewEVM(context, txContext, statedb, test.Genesis.Config, vm.Config{Tracer: tracer})
-			msg, err := core.TransactionToMessage(tx, signer, baseFee)
+			msg, err := core.TransactionToMessage(tx, signer, context.BaseFee)
 			if err != nil {
 				t.Fatalf("failed to prepare transaction for tracing: %v", err)
 			}
