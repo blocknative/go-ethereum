eac513ce943891328678e02020c70a42736b0a18
Ayaz Abbas <lfcayaz@gmail.com>
2023-07-06 16:14:49 +0100

    remove unused imports

--- core/state_processor.go
+++ core/state_processor.go
@@ -29,8 +29,6 @@ import (
 	"github.com/ethereum/go-ethereum/core/vm"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/eth/tracers/blocknative"
-	"github.com/ethereum/go-ethereum/eth/tracers/logger"
-	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/params"
 )
 
