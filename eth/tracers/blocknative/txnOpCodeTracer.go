6a8ee158d71e282f498784b557e33b82d752054c
Tyler Smith <mail@tcry.pt>
2023-09-10 04:40:45 -0400

    tweak: defer nbc calculation until GetTrace.

--- eth/tracers/blocknative/txnOpCodeTracer.go
+++ eth/tracers/blocknative/txnOpCodeTracer.go
@@ -61,6 +61,11 @@ func NewTxnOpCodeTracerWithOpts(opts TracerOpts) (Tracer, error) {
 
 // GetTrace returns the resulting Trace object.
 func (t *txnOpCodeTracer) GetTrace() (*Trace, error) {
+	// Get the final balance changes
+	if t.opts.BalanceChanges {
+		t.trace.NetBalanceChanges = t.balanceTracker.calculateNetBalanceChanges()
+	}
+
 	// Only want the top level trace, all other indexes hold subtraces to which we do not particularly need
 	t.trace.CallFrame = t.callStack[0]
 	return &t.trace, nil
@@ -236,16 +241,11 @@ func (t *txnOpCodeTracer) CaptureExit(output []byte, gasUsed uint64, err error) 
 
 func (t *txnOpCodeTracer) CaptureTxStart(_ uint64) {}
 
+func (t *txnOpCodeTracer) CaptureTxEnd(_ uint64) {}
+
 // SetStateRoot implements core.stateRootSetter and stores the given root in the trace's BlockContext.
 func (t *txnOpCodeTracer) SetStateRoot(root common.Hash) {
 	t.trace.BlockContext.StateRoot = bytesToHex(root.Bytes())
-}
-
-func (t *txnOpCodeTracer) CaptureTxEnd(_ uint64) {
-	// Get the final balance changes
-	if t.opts.BalanceChanges {
-		t.trace.NetBalanceChanges = t.balanceTracker.calculateNetBalanceChanges()
-	}
 }
 
 // Stop terminates execution of the tracer at the first opportune moment.
