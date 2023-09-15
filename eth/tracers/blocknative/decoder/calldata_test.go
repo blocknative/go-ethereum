a3788645568e1f7fdc06787822948aef36c672cb
Tyler Smith <mail@tcry.pt>
2023-09-14 20:52:09 -0400

    tests: Fix calldata decoding test.

--- eth/tracers/blocknative/decoder/calldata_test.go
+++ eth/tracers/blocknative/decoder/calldata_test.go
@@ -24,12 +24,11 @@ func TestDecodeCalldata(t *testing.T) {
 				[]byte{35, 184, 114, 221, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 84, 112, 197, 166, 252, 231, 68, 122, 253, 44, 155, 227, 160, 242, 94, 54, 44, 9, 54, 97, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 71, 158, 224, 54, 58, 122, 194, 239, 52, 203, 167, 238, 130, 210, 194, 224, 101, 45, 70, 105, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 24, 197},
 			},
 			&DecodedCall{
-				CallType:  AssetTransfer,
-				AssetType: AssetTypeERC721,
-				From:      common.HexToAddress("0x5470c5a6Fce7447aFd2C9BE3A0F25e362C093661"),
-				To:        common.HexToAddress("0x479ee0363a7Ac2ef34cba7ee82D2C2E0652D4669"),
-				Value:     big.NewInt(1),
-				TokenID:   nil,
+				CallType: AssetTransfer,
+				From:     common.HexToAddress("0x5470c5a6Fce7447aFd2C9BE3A0F25e362C093661"),
+				To:       common.HexToAddress("0x479ee0363a7Ac2ef34cba7ee82D2C2E0652D4669"),
+				Value:    big.NewInt(6341),
+				TokenID:  nil,
 			}},
 	}
 	for _, tt := range tests {
