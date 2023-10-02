6815d48a779f5d483c1ddcc876681cc919bc39ab
Tyler Smith <mail@tcry.pt>
2023-10-02 11:01:22 -0400

    tweak: Don't add logs to full block traces; they're in receipts.

--- eth/filters/trace_api.go
+++ eth/filters/trace_api.go
@@ -32,7 +32,6 @@ var defaultTxTraceOpts = blocknative.TracerOpts{
 var defaultBlockTraceOpts = blocknative.TracerOpts{
 	BalanceChanges:      true,
 	DisableBlockContext: true,
-	Logs:                true,
 }
 
 // TraceNewPendingTransactions creates a subscription that is triggered each time a
