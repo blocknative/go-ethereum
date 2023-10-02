7b3d6637c962e435a1a5a05ad7cfcca12f66519d
Tyler Smith <mail@tcry.pt>
2023-10-02 09:19:15 -0400

    tweak: Add logs to staging trace stream.

--- eth/filters/trace_api.go
+++ eth/filters/trace_api.go
@@ -25,11 +25,13 @@ import (
 
 var defaultTxTraceOpts = blocknative.TracerOpts{
 	BalanceChanges: true,
+	Logs:           true,
 }
 
 var defaultBlockTraceOpts = blocknative.TracerOpts{
 	BalanceChanges:      true,
 	DisableBlockContext: true,
+	Logs:                true,
 }
 
 // TraceNewPendingTransactions creates a subscription that is triggered each time a
