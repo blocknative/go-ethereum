af14ba546a8c6ea5f93b046b2ad6ea31612a4eea
Tyler Smith <mail@tcry.pt>
2023-10-08 05:29:13 -0400

    tweak: Add hostname to trace timers.

--- eth/filters/trace_api.go
+++ eth/filters/trace_api.go
@@ -109,7 +109,7 @@ func (api *FilterAPI) NewPendingTransactionsWithTrace(ctx context.Context, trace
 					}
 
 					traceCtx.TxHash = tx.Hash()
-					timer := prometheus.NewTimer(metricsTraceTxTimer.With(nil))
+					timer := newTimer(metricsTraceTxTimer)
 					trace, err := traceTx(msg, traceCtx, blockCtx, chainConfig, statedb, tracerOpts)
 					if err != nil {
 						log.Error("pending_txs_stream: failed to trace tx", "err", err, "tx", tx.Hash())
@@ -309,7 +309,7 @@ func traceBlock(block *types.Block, chainConfig *params.ChainConfig, chain *core
 		results   = make([]*blocknative.Trace, len(txs))
 	)
 
-	timer := prometheus.NewTimer(metricsTraceBlockTimer.With(nil))
+	timer := newTimer(metricsTraceBlockTimer)
 	for i, tx := range txs {
 		msg, err := core.TransactionToMessage(tx, signer, block.BaseFee())
 		if err != nil {
@@ -342,4 +342,8 @@ func getTracerOpts(optsJSON *[]byte, defaults blocknative.TracerOpts) (blocknati
 		}
 	}
 	return opts, nil
+}
+
+func newTimer(histogram *prometheus.HistogramVec) *prometheus.Timer {
+	return prometheus.NewTimer(histogram.WithLabelValues(metricsHostName))
 }
