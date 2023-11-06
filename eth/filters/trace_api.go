650021e4229396362ed87767e56ad079c409b0ba
Tyler Smith <mail@tcry.pt>
2023-11-06 13:04:49 -0400

    tweak: Recover from any panics in trace streams.

--- eth/filters/trace_api.go
+++ eth/filters/trace_api.go
@@ -59,6 +59,14 @@ func (api *FilterAPI) NewPendingTransactionsWithTrace(ctx context.Context, trace
 		metricsPendingTxsNew.Inc(1)
 		defer metricsPendingTxsEnd.Inc(1)
 
+		// Recover from any panics. Should be the last deferred call so it runs
+		// first.
+		defer func() {
+			if r := recover(); r != nil {
+				log.Error("pending_txs_stream panic:", r)
+			}
+		}()
+
 		for {
 			select {
 			case txs := <-txs:
@@ -176,6 +184,14 @@ func (api *FilterAPI) NewFullBlocksWithTrace(ctx context.Context, tracerOptsJSON
 
 		metricsBlocksNew.Inc(1)
 		defer metricsBlocksEnd.Inc(1)
+
+		// Recover from any panics. Should be the last deferred call so it runs
+		// first.
+		defer func() {
+			if r := recover(); r != nil {
+				log.Error("block_stream panic:", r)
+			}
+		}()
 
 		var hashes []common.Hash
 		for {
