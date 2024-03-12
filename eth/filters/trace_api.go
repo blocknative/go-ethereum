d24d4cd45238a0b0f32911b4bed52d2b9c4e7908
Łukasz Miłkowski <lukanus@uaznia.net>
2024-03-12 13:04:16 +0100

    standard tracer

--- eth/filters/trace_api.go
+++ eth/filters/trace_api.go
@@ -262,22 +262,38 @@ func traceTx(message *core.Message, txCtx *tracers.Context, vmctx vm.BlockContex
 // traceBlockTx traces a transaction with the given contexts.
 func traceBlockTx(message *core.Message, txCtx *tracers.Context, vmctx vm.BlockContext, chainConfig *params.ChainConfig, statedb *state.StateDB, tracerOpts blocknative.TracerOpts) (*core.ExecutionResult, *blocknative.Trace, error) {
 
-	tracerOpts.DisableBlockContext = false
-	tracer, err := blocknative.NewTracerWithOpts(tracerOpts)
+	tracer, err := tracers.DefaultDirectory.New("callTracer", txCtx, nil)
 	if err != nil {
 		return nil, nil, err
 	}
 
+	// tracerOpts.DisableBlockContext = false
+	// tracer, err := blocknative.NewTracerWithOpts(tracerOpts)
+	// if err != nil {
+	// 	return nil, nil, err
+	// }
+
 	vmenv := vm.NewEVM(vmctx, core.NewEVMTxContext(message), statedb, chainConfig, vm.Config{Tracer: tracer, NoBaseFee: false})
 	statedb.SetTxContext(txCtx.TxHash, txCtx.TxIndex)
 	result, err := core.ApplyMessage(vmenv, message, new(core.GasPool).AddGas(message.GasLimit))
+	// if err != nil {
+	// 	return result, nil, fmt.Errorf("tracing failed: %w", err)
+	// }
+	// trace, err := tracer.GetTrace()
+	// if err != nil {
+	// 	return nil, nil, err
+	// }
+
 	if err != nil {
 		return result, nil, fmt.Errorf("tracing failed: %w", err)
 	}
-	trace, err := tracer.GetTrace()
+	traceJSON, err := tracer.GetResult()
 	if err != nil {
 		return nil, nil, err
 	}
+
+	trace := &blocknative.Trace{}
+	err = json.Unmarshal(traceJSON, trace)
 
 	return result, trace, err
 }
