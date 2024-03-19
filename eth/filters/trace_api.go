f78844032d6ecca72ed9ab0c8d31a5154b866447
Łukasz Miłkowski <lukanus@uaznia.net>
2024-03-19 18:13:18 +0100

    move it to the bottom

--- eth/filters/trace_api.go
+++ eth/filters/trace_api.go
@@ -85,13 +85,6 @@ func (api *FilterAPI) NewPendingTransactionsWithTrace(ctx context.Context, trace
 					header.ExcessBlobGas = &ex
 				}
 
-				sDB, err := api.sys.chain.State()
-				if err != nil {
-					log.Error("failed to get state", "err", err)
-					notifier.Notify(rpcSub.ID, tracedTxs)
-					return
-				}
-
 				for _, tx := range txs {
 					rpcTx := newRPCPendingTransaction(tx)
 					if rpcTx == nil {
@@ -108,6 +101,12 @@ func (api *FilterAPI) NewPendingTransactionsWithTrace(ctx context.Context, trace
 					tracedTxs = append(tracedTxs, rpcTx)
 				}
 
+				sDB, err := api.sys.chain.State()
+				if err != nil {
+					log.Error("failed to get state", "err", err)
+					notifier.Notify(rpcSub.ID, tracedTxs)
+					return
+				}
 				blockCtx := core.NewEVMBlockContext(header, api.sys.chain, nil)
 
 				for i, tx := range tracedTxs {
@@ -125,9 +124,6 @@ func (api *FilterAPI) NewPendingTransactionsWithTrace(ctx context.Context, trace
 					msg.GasFeeCap = common.Big0     // skip the check of ErrFeeCapTooLow
 					msg.GasTipCap = common.Big0     // skip the check of ErrFeeCapTooLow
 
-					// if i > 0 && snapID > 0 {
-					// 	statedb.RevertToSnapshot(snapID)
-					// }
 					traceCtx.TxHash = tx.Hash
 					tx.Trace, err = traceTx(msg, traceCtx, blockCtx, chainConfig, sDB.Copy(), tracerOpts)
 					if err != nil {
