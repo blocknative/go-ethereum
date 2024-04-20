77d08568313695dee8207258dfec1b1bf83b545b
Tyler Smith <mail@tcry.pt>
2024-04-20 05:59:38 -0400

    logging

--- eth/backend.go
+++ eth/backend.go
@@ -279,13 +279,16 @@ func New(stack *node.Node, config *ethconfig.Config) (*Ethereum, error) {
 	}
 
 	// Start the redis RPC service
+	log.Info("REDIS RPC", "addr", config.RedisRPCAddr, "db", config.RedisRPCDB)
 	if config.RedisRPCAddr != "" {
+		log.Info("Starting Redis RPC")
 		redisClient := redis.NewClient(&redis.Options{
 			Addr: config.RedisRPCAddr,
 			DB:   config.RedisRPCDB,
 		})
 		eth.redisRPCService = redisRPC.New(redisClient, eth.APIBackend)
 		eth.redisRPCService.Start()
+		log.Info("Redis RPC started")
 	}
 
 	// Start the RPC service
